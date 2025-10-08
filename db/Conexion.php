e2d76285d310b5e4d167be115dbcaae11139456a
DavidLP2 <112593419+DavidLP2@users.noreply.github.com>
2025-10-08 11:28:31 -0500

    Add files via upload

---
+++ db/Conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // Cambia "localhost" si es necesario
+$username = "root"; // Cambia "root" si es necesario
+$password = ""; // Cambia la contraseña si es necesario
+$dbname = "ejemplo_sem"; // Cambia "ejemplo" por el nombre de tu base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
